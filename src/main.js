3b8a52b36f9236edeb1c944e1c0114abe215ba31
theusmon0v <asadbek.usmonoff2003@gmail.com>
2024-06-03 19:38:42 +0500

    loyiha tugatildi

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import ErrorHandle from './shared/errors/errorStatus.js';
 import mainRouter, { swaggerSpec } from './_api.js';
 import cookieParser from 'cookie-parser';
 import { modelSync, relation } from './utils/relation.js';
-
+import path from "path"
 const app = express();
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
