3c5a32765970f3149bed68cedcb5df89e9c8d4de
theusmon0v <asadbek.usmonoff2003@gmail.com>
2024-06-03 15:26:54 +0500

    loyiha tugatildi

--- src/_api.js
+++ src/_api.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import {Router} from "express";
 import authRouter from "./modules/auth/_api.js";
 import categoryRouter from "./modules/category/_api.js";
 import productRouter from "./modules/Products/_api.js";
 import userAddCommetRouter from "./modules/commets/_api.js";
 import orderRouter from "./modules/order/_api.js";
-import swaggerJSDoc from "swagger-jsdoc";
+import swaggerJSDoc from "swagger-jsdoc"
 import adminRouter from "./modules/user/_api.js";
 
 const mainRouter = Router();
@@ -14,29 +14,30 @@ mainRouter.use("/", categoryRouter);
 mainRouter.use("/", productRouter);
 mainRouter.use("/", userAddCommetRouter);
 mainRouter.use("/", orderRouter);
-mainRouter.use("/", adminRouter);
+mainRouter.use("/", adminRouter)
+
+
 
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Bu mebel sayti uchun swagger api hisoblanadi :)",
-      version: "1.0.0",
-      description: "Description of your API",
-    },
-    servers: [
-      {
-        url: "https://furniture-imoe.onrender.com",
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Bu mebel sayti uchun swagger api hisoblanadi :)',
+        version: '1.0.0',
+        description: 'Description of your API',
       },
-    ],
-  },
-  apis: [
-    "./src/modules/auth/_api.js",
-    "./src/modules/category/_api.js",
-    "./src/modules/Products/_api.js",
-  ],
-};
-
+      servers: [
+        {
+          url: 'https://furniture-imoe.onrender.com/',
+        },
+      ],
+    },
+    apis: ['./src/modules/auth/_api.js', './src/modules/category/_api.js', './src/modules/Products/_api.js'], 
+  
+  };
+  
 export const swaggerSpec = swaggerJSDoc(options);
+
+
 
 export default mainRouter;
